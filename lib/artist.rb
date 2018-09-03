d933eb348916a34d88a68d5d1e06c162cb06ce97
Adam Clayman <adam@clayman.org>
2018-09-03 13:14:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,20 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     @name = name
     @@all < self
+    @songs = []
   end
 
   def self.all
     @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.artist = self
   end
 
 
