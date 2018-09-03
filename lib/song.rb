f1d916cd0a46883a15e27d511fb9b72366ef8ab2
Adam Clayman <adam@clayman.org>
2018-09-03 13:19:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 class Song
   @@all = []
-  attr_accessor :name, :genre, :artist
+  attr_accessor :name, :artist, :genre
 
-  def initialize(name, genre)
+  def initialize(name, artist, genre)
     @name = name
+    @artist = artist
     @genre = genre
   end
 
