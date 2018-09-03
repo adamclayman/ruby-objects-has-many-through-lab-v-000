344983b0a9f11ed28221c966fa41ae294a8178c1
Adam Clayman <adam@clayman.org>
2018-09-03 13:25:27 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -9,9 +9,15 @@ class Genre
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
 
   def new_song(name, artist)
     song = Song.new(name, artist, self)
+    @songs << song
+  end
+
+  def songs
+    self.all.select {|genre| genre}
   end
 end
